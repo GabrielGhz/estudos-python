c2eeb0538fad697eb37d9a02472056396744d7b2
Gb <joaogabrielmcpe110@gmail.com>
2022-09-13 10:49:50 -0300

    somar e mutiplicar

--- operacao_matematica.py
+++ operacao_matematica.py
@@ -1,1 +1,7 @@
 '''realizar operação matemática de soma e multiplicaçaõ quando o usuário pedir'''
+
+numero1 = int(input("informe um numero"))
+numero2 = int(input("informe um numero"))
+resultado_soma = numero1 + numero2
+resultado_multiplicacao = numero1 * numero2
+print("a soma foi", resultado_soma, "e a multiplcacao foi", resultado_multiplicacao)
